249165b5a5ca2bdb75d39fab57d673a799c16815
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-22 21:42:02 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { useRef } from "react";
 
 const initialData = [
-  { id: 1, name: "Dan", age: 25 },
-  { id: 2, name: "Shyah", age: 30 },
+  { id: 1, name: "Ram", age: 25 },
+  { id: 2, name: "Shyam", age: 30 },
   { id: 3, name: "Ali", age: 35 },
   { id: 4, name: "Shaw", age: 20 },
   { id: 5, name: "Tavneet", age: 50 },
@@ -12,8 +12,8 @@ const initialData = [
 const App = () => {
   const inputRef = useRef({});
 
-  function handleSave(){
-    const updatedId = []
+  function handleSave() {
+    const updatedId = [];
     initialData.forEach((row) => {
       const nameRef = inputRef.current[row.id]?.name;
       const ageRef = inputRef.current[row.id]?.age;
@@ -21,59 +21,67 @@ const App = () => {
       const currentName = nameRef?.value;
       const currentAge = ageRef?.value;
 
-      if(currentName !== row.name || Number(currentAge) !== row.age){
-        updatedId.push(row.id)
+      if (currentName !== row.name || Number(currentAge) !== row.age) {
+        updatedId.push(row.id);
       }
-    })
-    if(updatedId.length > 0){
-      console.log("Edited rows: ",updatedId);
+    });
+    if (updatedId.length > 0) {
+      console.log("Edited rows: ", updatedId);
     }
   }
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {initialData.map((row) => {
-            if (!inputRef.current[row.id]) {
-              inputRef.current[row.id] = {
-                name: React.createRef(),
-                age: React.createRef(),
-              };
-            }
+      <h2>Track edited cells to log updates for future</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSave();
+        }}
+      >
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {initialData.map((row) => {
+              if (!inputRef.current[row.id]) {
+                inputRef.current[row.id] = {
+                  name: React.createRef(),
+                  age: React.createRef(),
+                };
+              }
 
-            return (
-              <tr key={row.id}>
-                <td>{row.id}</td>
-                <td>
-                  <input
-                    type="text"
-                    defaultValue={row.name}
-                    ref={(el) => (inputRef.current[row.id].name = el)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="number"
-                    defaultValue={row.age}
-                    ref={(el) => (inputRef.current[row.id].age = el)}
-                  />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <button onClick={handleSave}>Save changes</button>
+              return (
+                <tr key={row.id}>
+                  <td>{row.id}</td>
+                  <td>
+                    <input
+                      type="text"
+                      defaultValue={row.name}
+                      ref={(el) => (inputRef.current[row.id].name = el)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      defaultValue={row.age}
+                      ref={(el) => (inputRef.current[row.id].age = el)}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <button type="submit">Save changes</button>
+      </form>
     </div>
   );
 };
 
-export default App
+export default App;
